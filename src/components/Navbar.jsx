be5c7643789703afc9b2a338f73ca8f771709b6a
dantecd <dymadmin@dymconsultores.com.ar>
2022-02-14 14:45:46 -0300

    se corrigio nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,22 +2,22 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-light bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand"><img src="https://www.electoral.gob.ar/nuevo/img/Logo_cne_350x60px.png" alt="cne" /></a>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#"><img src="https://www.electoral.gob.ar/nuevo/img/Logo_cne_350x60px.png" alt="cne" /></a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        </ul>
         <form className="d-flex">
-          <input
-            class="form-control me-2"
-            type="search"
-            placeholder="Buscar por provincias..."
-            aria-label="Search"
-          />
-          <button className="btn btn-outline-primary" type="submit">
-            Buscar
-          </button>
+          <input  className="form-control me-2" type="search" placeholder="Search" aria-label="Buscar por provincia..."/>
+          <button className="btn btn-outline-success" type="submit">Buscar</button>
         </form>
       </div>
-    </nav>
+    </div>
+  </nav>
   );
 };
 
