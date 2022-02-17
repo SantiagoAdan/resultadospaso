c405085abbc234ed6afc7cb63018879010cb7158
dantecd <dymadmin@dymconsultores.com.ar>
2022-02-17 15:49:18 -0300

    la busqueda funciona

--- src/components/Datos.js
+++ src/components/Datos.js
@@ -1,54 +1,56 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import JSONDATA from "../resultados.json";
+import { useState } from "react";
 
-function useDatos() {
-  const [resultados, setPostres] = useState([]);
-
-  useEffect(() => {
-    fetch("json/resultados.json")
-      .then((response) => response.json())
-      .then((datos) => {
-        setPostres(datos);
-      });
-  }, []);
-
-  return resultados;
-}
-
-export default function Datos() {
-  const resultados = useDatos();
-
+function Datos() {
+  const [searchTerm, setSearchTem] = useState("");
   return (
-    <div className="container" align="center"><div class="dropdown">
-    <hr />
-    <p className="display-6">PARTIDOS GANADORES LEGISLATIVAS 2021 </p>
-    <hr />
-  </div>
-      <div className="row">
-        <div className="col-md-12">
-          <table className="table table-striped">
-            <thead className="thead-dark">
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">PROVINCIA</th>
-                <th scope="col">PARTIDO</th>
-                <th scope="col">PORCENTAJE</th>
-                <th scope="col">VOTOS</th>
-              </tr>
-            </thead>
-            <tbody>
-              {resultados.map((item) => (
-                <tr key={item.id}>
-                  <td>{item.id}</td>
-                  <td>{item.provincia}</td>
-                  <td>{item.partido}</td>
-                  <td>{item.porcentaje}</td>
-                  <td>{item.votos}</td>
+    <div>
+      <input
+        className="form-control"
+        type="text"
+        placeholder="BUSCAR POR PROVINCIA/PARTIDO"
+        onChange={(event) => {
+          setSearchTem(event.target.value);
+        }}
+      />
+
+      {JSONDATA.filter((val) => {
+        if (searchTerm == "") {
+          return val;
+        } else if (
+          val.provincia.toLowerCase().includes(searchTerm.toLowerCase())
+        ) {
+          return val;
+        }
+      }).map((val, key) => {
+        return (
+          <div>
+            <table className="table text-center container" key={key}>
+              <thead>
+                <tr>
+                  <th scope="col">ID</th>
+                  <th scope="col">PROVINCIA</th>
+                  <th scope="col">PARTIDO</th>
+                  <th scope="col">PORCENTAJE</th>
+                  <th scope="col">VOTOS</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>{val.id}</th>
+                  <td>{val.provincia}</td>
+                  <td>{val.partido}</td>
+                  <td>{val.porcentaje}</td>
+                  <td>{val.votos}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        );
+      })}
     </div>
   );
 }
+
+export default Datos;
